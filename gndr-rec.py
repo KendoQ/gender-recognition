9403773925e4003840ae2f32d94b698ef1ae56ab
ddougl <douglas.danielj@gmail.com>
2022-11-03 13:12:19 -0400

    adding gndr-rec

---
+++ gndr-rec.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# File: gndr-rec.py
+
+# Author: Daniel Douglsa
+
+# Usage: python gndr-rec <wavfile>
+
+# Function imports
+from sys import argv
+from speech import Speech
+import subprocess
+import numpy
+
+# Main
+# Instantiate speech instance
+voice = Speech(argv[1])
+voice.description()
+
+print(voice.gender(20,2))
+subprocess.call(['rm', voice.wavname])
